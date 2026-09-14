e094c7852250fbc0701909345522161593f0b747
pauloryan07 <pauloryan418@gmail.com>
2026-09-14 16:36:39 -0300

    Primeiro commit

---
+++ assets/js/server.js
@@ -0,0 +1,84 @@
+const chapa1 = [
+    {
+        nome: "Russas ",
+        cargo: "Deputada(o) Federal",
+        Partido: "Vale do Jaguaribe",
+        numero: "25"    
+    },
+    {
+        nome: "Limoeiro do Norte",
+        cargo: "Deputada(o) Estadual",
+        Partido: "Vale do Jaguaribe",
+        numero: "31456"
+    },
+    {
+        nome: "Jaguaribe",
+        cargo: "Senadora(or) - 1ª vaga",
+        Partido: "Vale do Jaguaribe",
+        numero: "278"
+    },
+    {
+        nome: "Quixeré",
+        cargo: "Senadora(or) - 2ª vaga",
+        Partido: "Vale do Jaguaribe",
+        numero: "691"
+    },
+    {
+        nome: "Morada Nova",
+        cargo: "Governadora(or)",
+        Partido: "Vale do Jaguaribe",
+        numero: "37"
+    },
+    {
+        
+        nome: "Tabuleiro do Norte",
+        cargo: "Presidente da República",
+        Partido: "Vale do Jaguaribe",
+        numero: "21"
+    }
+];
+
+const chapa2 = {
+    numero: 2,
+    nome: "LITORAL LESTE",
+
+    candidatos: [
+        {
+            nome: "Aracati",
+            cargo: "Deputada(o) Federal",
+            Partido: "Litoral Leste",
+            numero: "45"
+        },
+        {
+            nome: "Beberibe",
+            cargo: "Deputada(o) Estadual",
+            Partido: "Litoral Leste",
+            numero: "67891"
+        },
+        {
+            nome: "Cascavel",
+            cargo: "Senadora(or) - 1ª vaga",
+            Partido: "Litoral Leste",
+            numero: "542"
+        },
+        {
+            nome: "Fortim",
+            cargo: "Senadora(or) - 2ª vaga",
+            Partido: "Litoral Leste",
+            numero: "783"
+        },
+        {
+            nome: "Icapuí",
+            cargo: "Governadora(or)",
+            Partido: "Litoral Leste",
+            numero: "56"
+        },
+        {
+            nome: "Pindoretama",
+            cargo: "Presidente da República",
+            Partido: "Litoral Leste",
+            numero: "18"
+        }
+    ]
+};
+
