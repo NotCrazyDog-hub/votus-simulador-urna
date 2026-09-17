1699325f6890ef88ee7b317afa88ff18949afc73
S0usaker <sousakerllon4@icloud.com>
2026-09-17 09:03:08 -0300

    feat: mudança nos nomes do candidatos

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,95 +1,62 @@
-// =====================================================
-// CHAPA 1
-// =====================================================
-
-const chapa1 = [
-    {
-        nome: "Russas",
-        cargo: "Deputada(o) Federal",
-        Partido: "Vale do Jaguaribe",
-        numero: "1234"
-    },
-    {
-        nome: "Limoeiro do Norte",
-        cargo: "Deputada(o) Estadual",
-        Partido: "Vale do Jaguaribe",
-        numero: "31456"
-    },
-    {
-        nome: "Jaguaribe",
-        cargo: "Senadora(or) - 1ª vaga",
-        Partido: "Vale do Jaguaribe",
-        numero: "278"
-    },
-    {
-        nome: "Quixeré",
-        cargo: "Senadora(or) - 2ª vaga",
-        Partido: "Vale do Jaguaribe",
-        numero: "691"
-    },
-    {
-        nome: "Morada Nova",
-        cargo: "Governadora(or)",
-        Partido: "Vale do Jaguaribe",
-        numero: "37"
-    },
-    {
-        nome: "Tabuleiro do Norte",
-        cargo: "Presidente da República",
-        Partido: "Vale do Jaguaribe",
-        numero: "21"
-    }
-];
-
-
-// =====================================================
-// CHAPA 2
-// =====================================================
-
 const chapa2 = {
-    numero: 2,
-    nome: "LITORAL LESTE",
 
-    candidatos: [
-        {
-            nome: "Aracati",
-            cargo: "Deputada(o) Federal",
-            Partido: "Litoral Leste",
-            numero: "5678"
-        },
-        {
-            nome: "Beberibe",
-            cargo: "Deputada(o) Estadual",
-            Partido: "Litoral Leste",
-            numero: "67891"
-        },
-        {
-            nome: "Cascavel",
-            cargo: "Senadora(or) - 1ª vaga",
-            Partido: "Litoral Leste",
-            numero: "542"
-        },
-        {
-            nome: "Fortim",
-            cargo: "Senadora(or) - 2ª vaga",
-            Partido: "Litoral Leste",
-            numero: "783"
-        },
-        {
-            nome: "Icapuí",
-            cargo: "Governadora(or)",
-            Partido: "Litoral Leste",
-            numero: "56"
-        },
-        {
-            nome: "Pindoretama",
-            cargo: "Presidente da República",
-            Partido: "Litoral Leste",
-            numero: "18"
-        }
-    ]
-};
+            numero: 2,
 
+            nome: "Docente",
+
+            candidatos: [
+
+                {
+                    nome: "Elenir",
+                    cargo: "Deputada(o) Federal",
+                    Partido: "Docente",
+                    numero: "5678",
+                foto: "Elenir.png"
+                },
+
+                {
+                    nome: "Rosangela",
+                    cargo: "Deputada(o) Estadual",
+                    Partido: "Docente",
+                    numero: "67891",
+                foto: "Rosangela.png"
+                },
+
+                {
+                    nome: "Israely",
+                    cargo: "Senadora(or) - 1ª vaga",
+                    Partido: "Docente",
+                    numero: "542",
+                foto: "Israely.png"
+                },
+
+                {
+                    nome: "Silvania",
+                    cargo: "Senadora(or) - 2ª vaga",
+                    Partido: "Docente",
+                    numero: "783",
+                foto: "Silvania.png"
+                },
+
+                {
+                    nome: "Natan",
+                    cargo: "Governadora(or)",
+                    Partido: "Docente",
+                    numero: "56",
+                foto: "Natan.png"
+                },
+
+                {
+                    nome: "Rusilânia",
+                    cargo: "Presidente da República",
+                    Partido: "Docente",
+                    numero: "18",
+                foto: "Rusilania.png"
+                }
+
+            ]
+
+        };
 
 // =====================================================
 // TODOS OS CANDIDATOS
